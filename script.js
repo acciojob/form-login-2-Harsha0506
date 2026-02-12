64a3721a0dd027485764c851a29ed8ca1d908ce8
Harsha0506 <110712960+Harsha0506@users.noreply.github.com>
2026-02-13 03:43:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+let submit = document.getElementById("button");
+
+submit.addEventListener("click", function () {
+  let firstName = document.getElementById("firstName").value;
+  let lastName = document.getElementById("lastName").value;
+  let phone = document.getElementById("phone").value;
+  let email = document.getElementById("email").value;
+
+  alert(
+    "First Name: " + firstName +
+    " Last Name: " + lastName +
+    " Phone Number: " + phone +
+    " Email ID: " + email
+  );
+});
+
